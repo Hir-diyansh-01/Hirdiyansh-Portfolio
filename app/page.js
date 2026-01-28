aad343cbbfce5b74f9b9632791bc84b2be464087
Hir-diyansh-01 <hirdiyanshmishra@gmail.com>
2026-01-28 13:44:35 +0530

    Update README and homepage content

--- app/page.js
+++ app/page.js
@@ -1,24 +1,25 @@
+'use client';
 import { personalData } from "@/utils/data/personal-data";
 import AboutSection from "./components/homepage/about";
-import Blog from "./components/homepage/blog";
+// import Blog from "./components/homepage/blog";
 import ContactSection from "./components/homepage/contact";
 import Education from "./components/homepage/education";
-import Experience from "./components/homepage/experience";
+// import Experience from "./components/homepage/experience";
 import HeroSection from "./components/homepage/hero-section";
 import Projects from "./components/homepage/projects";
 import Skills from "./components/homepage/skills";
-import blogData from '../utils/data/blogData.json';
+// import blogData from '../utils/data/blogData.json';
 
-async function getData() {
-  const data = blogData;
+// async function getData() {
+//   const data = blogData;
 
-  const filtered = data.filter((item) => item?.cover_image).sort(() => Math.random() - 0.5);
+//   const filtered = data.filter((item) => item?.cover_image).sort(() => Math.random() - 0.5);
 
-  return filtered;
-};
+//   return filtered;
+// };
 
-export default async function Home() {
-  const blogs = await getData();
+export default  function Home() {
+  // const blogs = await getData();
 
   return (
     <div suppressHydrationWarning >
